352c94a95c3d19c1dcf8c1cade95f83bde3b0ae6
LevianVian <artalevi4@gmail.com>
2025-11-13 11:20:27 +0700

    Creating experiment 2, implementation array with score input and output functionality

---
+++ arrayValue12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class arrayValue12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] finalscore = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter the final score " + i + ": ");
+            finalscore[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Final score " + i + ": " + finalscore[i]);
+        }
+
+        sc.close();
+    }
+}
